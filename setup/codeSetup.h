56669fff73db7ec7d695cc37cb8fb587cc039415
alons126 <alonsportes@mail.tau.ac.il>
2024-09-17 09:08:25 +0300

    Q2 study - C12 @4GeV NO cuts - redo2

--- setup/codeSetup.h
+++ setup/codeSetup.h
@@ -107,17 +107,17 @@ std::string file_name = "general_file";
 // std::string AnalyseFile = AnalyseFileDir + "*.hipo"; // all run files
 
 
-/* C12 2.07052GeV - run 015664 (ifarm DATA files - C12 DATA files) */
-std::string AnalyseFilePath = "cache/clas12/rg-m/production/pass1/2gev/C/dst/recon";
-std::string AnalyseFileSample = "015664";
-std::string AnalyseFileDir = "/" + AnalyseFilePath + "/" + AnalyseFileSample + "/";
-std::string AnalyseFile = AnalyseFileDir + "*.hipo"; // all run files
-
-// /* C12 4.02962GeV - run 015778 (ifarm DATA files - C12 DATA files) */
-// std::string AnalyseFilePath = "cache/clas12/rg-m/production/pass1/4gev/C/dst/recon";
-// std::string AnalyseFileSample = "015778";
+// /* C12 2.07052GeV - run 015664 (ifarm DATA files - C12 DATA files) */
+// std::string AnalyseFilePath = "cache/clas12/rg-m/production/pass1/2gev/C/dst/recon";
+// std::string AnalyseFileSample = "015664";
 // std::string AnalyseFileDir = "/" + AnalyseFilePath + "/" + AnalyseFileSample + "/";
 // std::string AnalyseFile = AnalyseFileDir + "*.hipo"; // all run files
+
+/* C12 4.02962GeV - run 015778 (ifarm DATA files - C12 DATA files) */
+std::string AnalyseFilePath = "cache/clas12/rg-m/production/pass1/4gev/C/dst/recon";
+std::string AnalyseFileSample = "015778";
+std::string AnalyseFileDir = "/" + AnalyseFilePath + "/" + AnalyseFileSample + "/";
+std::string AnalyseFile = AnalyseFileDir + "*.hipo"; // all run files
 
 // /* C12x4 5.98636GeV - run 015188 (ifarm DATA files - C12x4 DATA files) */
 // std::string AnalyseFilePath = "cache/clas12/rg-m/production/pass1/6gev/Cx4/dst/recon";
