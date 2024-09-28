fd043b2be659366e83e2c94d22a52e47b60fe7eb
alons126 <alonsportes@mail.tau.ac.il>
2024-09-28 14:26:29 +0300

    uniform 2GeV 10M full chainRun v2 run 2 amaps

--- Old_analysers/DetectorSimulationAnalyser.C
+++ Old_analysers/DetectorSimulationAnalyser.C
@@ -195,12 +195,12 @@ void EventAnalyser(const string &AnalyseFilePath, const string &AnalyseFileSampl
 
     // My analysis cuts ---------------------------------------------------------------------------------------------------------------------------------------------------
     /* Nucleon cuts */
-    bool apply_nucleon_cuts = false; // set as true to get good protons and calculate upper neutron momentum th.
+    bool apply_nucleon_cuts = true; // set as true to get good protons and calculate upper neutron momentum th.
 
     /* Physical cuts */
-    bool apply_nucleon_physical_cuts = false; // nucleon physical cuts master
+    bool apply_nucleon_physical_cuts = true; // nucleon physical cuts master
     // TODO: automate adding upper mom. th. to nucleon cuts (for nRes calc)
-    bool apply_nBeta_fit_cuts = false; // apply neutron upper mom. th.
+    bool apply_nBeta_fit_cuts = true; // apply neutron upper mom. th.
     bool apply_fiducial_cuts = false;
     bool apply_kinematical_cuts = false;
     bool apply_kinematical_weights = false;
@@ -245,7 +245,7 @@ void EventAnalyser(const string &AnalyseFilePath, const string &AnalyseFileSampl
 
     if (!apply_nucleon_cuts)
     {
-        apply_nucleon_physical_cuts = false;
+        Generate_AMaps = apply_nucleon_physical_cuts = false;
     }
 
     if (!apply_nucleon_physical_cuts)
