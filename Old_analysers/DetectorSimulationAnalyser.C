6d9541b40a213e53cc5f2ffe912ee43fd8175b04
alons126 <alonsportes@mail.tau.ac.il>
2024-10-01 14:36:52 +0300

    uniform 2GeV 10M full chainRun v3 run 1 only preselection cuts

--- Old_analysers/DetectorSimulationAnalyser.C
+++ Old_analysers/DetectorSimulationAnalyser.C
@@ -174,7 +174,7 @@ void EventAnalyser(const string &AnalyseFilePath, const string &AnalyseFileSampl
     // clas12ana cuts ---------------------------------------------------------------------------------------------------------------------------------------------------
     bool apply_cuts = false;                 // master ON/OFF switch for applying cuts
     bool clas12ana_particles = true;         // TODO: move form here!
-    bool only_preselection_cuts = false;     // keep as false for regular runs!
+    bool only_preselection_cuts = true;     // keep as false for regular runs!
     bool only_electron_quality_cuts = false; // keep as false for regular runs!
 
     /* Preselection cuts (event cuts) */
