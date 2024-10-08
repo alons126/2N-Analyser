c535cf42324e3c4c38f38ab798b35c4c34e123b1
alons126 <alonsportes@mail.tau.ac.il>
2024-10-08 14:55:51 +0300

    debugger test

--- Old_analysers/DetectorSimulationAnalyser.C
+++ Old_analysers/DetectorSimulationAnalyser.C
@@ -21,7 +21,8 @@ scp -r asportes@ftp.jlab.org:/w/hallb-scshelf2102/clas12/asportes/recon_c12_6gev
 #include "../source/classes/hPlots/hPlot1D.cpp"
 #include "../source/classes/hPlots/hPlot2D.cpp"
 
-#if MOMENTUMRESOLUTION_H == false
+#if defined(MOMENTUMRESOLUTION_H)
+#else
 #include "../source/classes/MomentumResolution/MomentumResolution.cpp"
 #endif
 
