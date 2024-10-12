d6fe5644cc0ac8bc5bb07484568fdd953c8f947b
alons126 <alonsportes@mail.tau.ac.il>
2024-10-12 17:40:28 +0300

    Uniform 2,4,6 GeV full NC 20s100b

--- Old_analysers/DetectorSimulationAnalyser.C
+++ Old_analysers/DetectorSimulationAnalyser.C
@@ -1075,7 +1075,7 @@ void EventAnalyser(const string &AnalyseFilePath, const string &AnalyseFileSampl
     }
 
     /* Set Bins by case */
-    int NumberNucOfMomSlices = 20, NumberElecOfMomSlices = 20, HistElectronSliceNumOfXBins = 75, HistNucSliceNumOfXBins = 75;
+    int NumberNucOfMomSlices = 20, NumberElecOfMomSlices = 20, HistElectronSliceNumOfXBins = 100, HistNucSliceNumOfXBins = 100;
 
     AMaps aMaps, wMaps;
     // TODO: UPDATE AMaps loading constructor electron histogram's number of bins
--- source/classes/Settings/CustomPlotsDirectory.h
+++ source/classes/Settings/CustomPlotsDirectory.h
@@ -29,7 +29,7 @@ private:
     string Added_PreStatuses, Added_Statuses;
 
     // string Global_Run_Status = "ChainRunV5_R2";
-    string Global_Run_Status = "ChainRunV5_R2_20s75b";
+    string Global_Run_Status = "ChainRunV5_R2_20s100b";
 
 public:
     // Default constructor ---------------------------------------------------------------------------------------------------------------------------------------------
